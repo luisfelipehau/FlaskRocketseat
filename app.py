1d92c18678493e431ff0c10f1fda284d78d47199
Luis Felipe Hau <luisfelipehau@gmail.com>
2024-04-15 18:49:16 -0300

    crud inicial, delete

--- app.py
+++ app.py
@@ -46,6 +46,8 @@ def update_task(id):
     for t in tasks:
         if t.id == id:
             task = t
+            break
+
     if task == None:
         return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
 
@@ -54,6 +56,21 @@ def update_task(id):
     task.description = data["description"]
     task.completed = data["completed"]
     return jsonify({"message": "Tarefa atualizada com sucesso"})
+
+
+@app.route("/tasks/<int:id>", methods=["DELETE"])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+
+        if not task:
+            return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+
+        tasks.remove(task)
+        return jsonify({"message": "Tarefa deletada com sucesso"})
 
 
 if __name__ == "__main__":
